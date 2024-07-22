9ca931136f1e48787602271b413e8c5c988ac86c
SaladKochan <cabbagekochan@gmail.com>
2024-07-22 20:53:59 +0300

    Залил нормальный сайт
    Попытка в нормальный сайт

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("myButton").addEventListener("click", function(event) {
+        event.preventDefault();  // Предотвращает переход по ссылке
+        alert("Кнопка нажата!"); // Пример действия
+        // Здесь можно добавить другие действия, которые должны выполняться при нажатии
+    });
